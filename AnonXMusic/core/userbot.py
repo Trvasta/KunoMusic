303476d573afe8471ef7d62f725ae42c8609c6a5
Trvasta <132353081+Trvasta@users.noreply.github.com>
2024-06-09 20:22:01 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("College_wali_masti")
-                await self.one.join_chat("Saykkunomusic")
+                await self.one.join_chat("OrboroBotSupport")
+                await self.one.join_chat("OrboroBotSupport")
             except:
                 pass
             assistants.append(1)
